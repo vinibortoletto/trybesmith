54f0588480920d01936adf4b3b7c3b4ac25f0127
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-06 17:48:20 -0300

    refactor: controller now use service

--- src/controllers/Product.controller.ts
+++ src/controllers/Product.controller.ts
@@ -1,17 +1,24 @@
 import { Request, Response } from 'express';
-import connection from '../models/connection';
-import ProductModel from '../models/Product.model';
+import ProductService from '../services/Product.service';
 
 export default class ProductController {
-  constructor(private productModel = new ProductModel(connection)) {}
+  constructor(private productService = new ProductService()) {}
 
   public create = async (req: Request, res: Response) => {
-    const newProduct = await this.productModel.create(req.body); 
-    res.status(201).json(newProduct);
+    try {
+      const newProduct = await this.productService.create(req.body); 
+      return res.status(201).json(newProduct);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   };
 
   public findAll = async (req: Request, res: Response) => {
-    const products = await this.productModel.findAll();
-    res.status(200).json(products);
+    try {
+      const products = await this.productService.findAll();
+      return res.status(200).json(products);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   };
-}
+} 

d76196d47ec776f59f33f4974ec2a89422f471a3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-06 16:40:20 -0300

    feat: find all products route

--- src/app.ts
+++ src/app.ts
@@ -6,5 +6,6 @@ const productController = new ProductController();
 
 app.use(express.json());
 app.post('/products', productController.create);
+app.get('/products', productController.findAll);
 
 export default app;

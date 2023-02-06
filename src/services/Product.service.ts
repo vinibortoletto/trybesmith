4d76fa71214845b85f0bd687f02745547942b769
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-06 17:45:56 -0300

    feat: create product service

---
+++ src/services/Product.service.ts
@@ -0,0 +1,12 @@
+import { IProduct } from '../interfaces/product.interface';
+import connection from '../models/connection';
+import ProductModel from '../models/Product.model';
+
+export default class ProductService {
+  constructor(private productModel = new ProductModel(connection)) {}
+
+  public async create(product: IProduct): Promise<IProduct> {
+    const newProduct = await this.productModel.create(product); 
+    return newProduct;
+  } 
+}

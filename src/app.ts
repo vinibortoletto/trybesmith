eaaa330a29f7c668ec303bcdec14c22974c64afc
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-06 16:30:11 -0300

    feat: create product route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import ProductController from './controllers/Product.controller';
 
 const app = express();
+const productController = new ProductController();
 
 app.use(express.json());
+app.post('/products', productController.create);
 
 export default app;

d49197cdf2caddcffd3bf2cebe0e28ad3be6b09c
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-06 17:32:08 -0300

    feat: create user route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import ProductController from './controllers/Product.controller';
+import UserController from './controllers/User.controller';
 
 const app = express();
+
 const productController = new ProductController();
+const userController = new UserController();
 
 app.use(express.json());
+
 app.post('/products', productController.create);
 app.get('/products', productController.findAll);
+
+app.post('/users', userController.create);
 
 export default app;

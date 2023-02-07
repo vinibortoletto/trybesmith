53559d0357a9da72246c911a74c80f30e9b0b203
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-07 17:44:53 -0300

    feat: user login service

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { IResultObject } from '../interfaces/resultObject.interface';
 import { IUser } from '../interfaces/user.interface';
 import connection from '../models/connection';
 import UserModel from '../models/User.model';
@@ -12,5 +13,19 @@ export default class UserService {
     await this.userModel.create(user);
     const token = jwt.sign(user, secret);
     return token;
+  }
+
+  public async login(username: string, password:string): Promise<IResultObject> {
+    const user = await this.userModel.findByUsername(username);
+    const error = { statusCode: 401, message: 'Username or password invalid' };
+
+    if (!user || user.password !== password) return error;
+
+    const token = jwt.sign(user, 'secret');
+
+    return {
+      statusCode: 200,
+      message: token,
+    };
   }
 }

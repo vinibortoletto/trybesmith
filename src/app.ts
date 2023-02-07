7a00e4645814655fed1c8649b5db62789c0220de
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-07 17:45:25 -0300

    feat: login route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+
 import OrderController from './controllers/Order.controller';
 import ProductController from './controllers/Product.controller';
 import UserController from './controllers/User.controller';
+
+import validateLogin from './middleware/validateLogin';
 
 const app = express();
 
@@ -16,5 +19,7 @@ app.get('/products', productController.findAll);
 
 app.post('/users', userController.create);
 app.get('/orders', orderController.findAll);
+
+app.post('/login', validateLogin, userController.login);
 
 export default app;

257c495537b73cf503253edc7c9edd5f063c1815
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-08 00:36:10 -0300

    feat: middleware import

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,13 @@
 import validateLogin from './validateLogin';
 import validateProducts from './validateProduct';
 import validateUser from './validateUser';
+import validateToken from './validateToken';
+import validateOrder from './validateOrder';
 
 export {
   validateLogin, 
   validateProducts,
   validateUser,
+  validateToken,
+  validateOrder,
 };

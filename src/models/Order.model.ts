ed4b8f452f7bbc7292d176d133f8f250e57c2545
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-08 00:36:54 -0300

    feat: create order and find order by id model

--- src/models/Order.model.ts
+++ src/models/Order.model.ts
@@ -1,14 +1,14 @@
-import { Pool, RowDataPacket } from 'mysql2/promise';
+import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
 import { IOrder } from '../interfaces/order.interface';
 
 export default class OrderModel {
   private connection: Pool;
 
-  constructor(connection:Pool) {
+  constructor(connection: Pool) {
     this.connection = connection;
   }
 
-  public async findAll():Promise<IOrder[]> {
+  public async findAll(): Promise<IOrder[]> {
     const query = `
       SELECT 
         o.id,
@@ -20,7 +20,39 @@ export default class OrderModel {
       GROUP BY o.id;
     `;
 
-    const [orders] = await this.connection.execute<IOrder[] & RowDataPacket[]>(query);
+    const [orders] = await this.connection.execute<IOrder[] & RowDataPacket[]>(
+      query,
+    );
     return orders;
+  }
+
+  public async create(
+    productsIds: number[],
+    userId: number | undefined,
+  ): Promise<void> {
+    const insertQuery = 'INSERT INTO Trybesmith.orders (user_id) VALUES (?)';
+
+    const [{ insertId: orderId }] = await this.connection.execute<ResultSetHeader>(
+      insertQuery,
+      [userId],
+    );
+
+    const updateQuery = 'UPDATE Trybesmith.products SET order_id = ? WHERE id = ?';
+    
+    await Promise.all(productsIds.map((id) => (
+      this.connection.execute(updateQuery, [orderId, id])
+    )));
+  }
+
+  public async findById(id: number): Promise<IOrder> {
+    const query = 'SELECT * FROM Trybesmith.orders WHERE id = ?';
+
+    const [[order]] = await this.connection.execute<(
+    IOrder & RowDataPacket)[]>(
+      query,
+      [id],
+      );
+
+    return order;
   }
 }

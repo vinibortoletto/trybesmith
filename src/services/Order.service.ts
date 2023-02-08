987baa767b6881bfc1d058bf26b51489fc55d2e4
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-08 00:35:29 -0300

    feat: create order service

--- src/services/Order.service.ts
+++ src/services/Order.service.ts
@@ -1,11 +1,21 @@
 import connection from '../models/connection';
 import OrderModel from '../models/Order.model';
+import UserModel from '../models/User.model';
 
 export default class OrderService {
-  constructor(private orderModel = new OrderModel(connection)) {}
+  constructor(
+    private orderModel = new OrderModel(connection),
+    private userModel = new UserModel(connection),
+  ) {}
 
   public async findAll() {
     const orders = await this.orderModel.findAll();
     return orders;
+  }
+
+  public async create(productsIds: number[], username:string) {
+    const { id: userId } = await this.userModel.findByUsername(username);
+    await this.orderModel.create(productsIds, userId); 
+    return { userId, productsIds };
   }
 }

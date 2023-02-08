22ec15f0eaeb61d07eec37eac19eecaa47b9ace2
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-08 00:37:06 -0300

    feat: create order route

--- src/routers/orders.router.ts
+++ src/routers/orders.router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { OrderController } from '../controllers';
+import { validateOrder, validateToken } from '../middleware';
 
 const router = Router();
 const orderController = new OrderController();
 
 router.get('/', orderController.findAll);
+router.post('/', validateToken, validateOrder, orderController.create);
 
 export default router;
